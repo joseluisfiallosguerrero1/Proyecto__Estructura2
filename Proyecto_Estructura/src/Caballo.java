2115961ea216e91df8c6d7d95a8ef412f6aa456e
joseluisfiallosguerrero1 <jose.fiallos@unitec.edu>
2016-05-25 21:24:54 -0600

    GUI

---
+++ Proyecto_Estructura/src/Caballo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author jose
+ */
+public class Caballo extends Pieza {
+
+    public Caballo() {
+    }
+
+    public Caballo(boolean Pieza) {
+        super(Pieza);
+    }
+
+    @Override
+    public String toString() {
+        return "Caballo{" + '}';
+    }
+    
+    
+}
---
+++ Proyecto_Estructura/src/Pieza.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author jose
+ */
+public class Pieza {
+    boolean Pieza;
+
+    public Pieza() {
+    }
+
+    public Pieza(boolean Pieza) {
+        this.Pieza = Pieza;
+    }
+
+    @Override
+    public String toString() {
+        return "Pieza{" + "Pieza=" + Pieza + '}';
+    }
+    
+}
